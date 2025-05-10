41dda96d5305734c82598d3575fc8992d64cf622
Daniel Pickering <danielpickering@Daniels-MacBook-Pro.local>
2025-05-10 10:20:24 +0100

    Add event code and username entry flow with admin support

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from events.models import Event
+from .models import Participant
 
 def enter_event_code(request):
     error = None
@@ -13,3 +14,20 @@ def enter_event_code(request):
             error = "Invalid code. Please try again."
 
     return render(request, 'users/enter_event_code.html', {'error': error})
+
+
+def enter_username(request, event_code):
+    event = get_object_or_404(Event, code__iexact=event_code)
+    error = None
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+
+        if not username:
+            error = "Please enter a username."
+        else:
+            # Optionally: check for duplicates
+            Participant.objects.create(username=username, event=event)
+            return redirect('waiting_room')  # We'll build this next
+
+    return render(request, 'users/enter_username.html', {'event': event, 'error': error})
