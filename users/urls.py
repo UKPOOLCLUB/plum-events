b374f0c19a690e024fec8d0cc3ff46b2d6a14f11
Daniel Pickering <danielpickering@Daniels-MacBook-Pro.local>
2025-05-10 10:20:24 +0100

    Add event code and username entry flow with admin support

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('enter/', views.enter_event_code, name='enter_event_code'),
+    path('join/<str:event_code>/', views.enter_username, name='enter_username'),
 ]
