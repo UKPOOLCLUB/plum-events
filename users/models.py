c04e915728294c619b906385735707ea4cf98c92
Daniel Pickering <danielpickering@Daniels-MacBook-Pro.local>
2025-05-12 17:15:01 +0100

    More updates to the scorecard

--- users/models.py
+++ users/models.py
@@ -4,10 +4,13 @@ import uuid
 
 class Participant(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=20)
     event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='participants')
     total_score = models.IntegerField(default=0)
     joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('username', 'event')
 
     def __str__(self):
         return self.username
