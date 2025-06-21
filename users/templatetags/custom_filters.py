6666f5adf29ecc05cdb17a9c21723e5b0175adf5
Daniel Pickering <danielpickering@Daniels-MacBook-Pro.local>
2025-06-21 14:35:45 +0100

    Adding a nav bar at the top

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.urls import reverse
 register = template.Library()
 
 @register.filter
@@ -19,3 +19,17 @@ def nested_dict_get(scores, player_id, hole):
         return scores.get(player_id, {}).get(hole, '')
     except:
         return ''
+
+@register.simple_tag
+def game_link(code, event):
+    if code == "mini_golf":
+        return reverse("my_golf_score_entry", args=[event.code])
+    elif code == "table_tennis":
+        return reverse("table_tennis_game_view", args=[event.id])
+    elif code == "pool_league":
+        return reverse("pool_league_view", args=[event.id])
+    elif code == "e_darts":
+        return reverse("enter_edarts_results", args=[event.id])
+    elif code == "killer_pool":
+        return reverse("killer_game", args=[event.id])
+    return "#"
