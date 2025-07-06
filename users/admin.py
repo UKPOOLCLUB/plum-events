59a314381ef98be7847155770ea9db88c645e7c9
Daniel Pickering <danielpickering@Daniels-MacBook-Pro.local>
2025-07-06 10:42:32 +0100

    Adding booking to admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Participant, EventAvailability
+from .models import Participant, EventAvailability, Booking
 
 admin.site.register(Participant)
 
@@ -9,3 +9,21 @@ class EventAvailabilityAdmin(admin.ModelAdmin):
     list_display = ('date', 'status')
     list_filter = ('status',)
     ordering = ('date',)
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'event_date',
+        'start_time',
+        'group_size',
+        'quote_total',
+        'paid',
+        'created_at',
+    )
+    list_filter = ('event_date', 'paid',)
+    search_fields = ('name', 'email')
+
+admin.site.register(Booking, BookingAdmin)
