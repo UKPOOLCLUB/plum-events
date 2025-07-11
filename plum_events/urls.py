844da398510811f7d0a511e380b6541ff82f4288
Daniel Pickering <contact@ukpoolclub.com>
2025-07-11 22:23:12 +0100

    Updating cancel payment

--- plum_events/urls.py
+++ plum_events/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),         # ✅ root handled by users app
-    path('users/', include('users.urls')),   # still fine to have this too
     path('events/', include('events.urls')),
 ]
 
